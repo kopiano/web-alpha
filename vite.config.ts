614753a250537b89e098d07c58ef9044e1a73d4e
coulsonzero <coulson2021@163.com>
2026-07-04 21:37:20 +0800

    [commit]: uopdate env

--- vite.config.ts
+++ vite.config.ts
@@ -15,13 +15,13 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5000,
     allowedHosts: true,
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        changeOrigin: true,
-        ws: true,
-      },
-    },
+    // proxy: {
+    //   "/api": {
+    //     target: "http://localhost:8000",
+    //     changeOrigin: true,
+    //     ws: true,
+    //   },
+    // },
   },
   resolve: {
     alias: {
